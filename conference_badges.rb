5ae7783fa4b4d1fdabc44a312b733508a6c45cfd
Sarah H <sarahrose.henderson@gmail.com>
2019-09-10 20:01:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,7 +8,5 @@ def batch_badge_creator(attendees)
 # badges = []
 attendees.each do {|name| return "Hello, my name is #{name}."}
  # badges << "Hello, my name is #{name}."
-
-end
 # return batch_badge_creator(badges)
 end
