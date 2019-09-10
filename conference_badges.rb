7ba13269ca884a03eec02ccaace2d23ee90cc2d1
Sarah H <sarahrose.henderson@gmail.com>
2019-09-10 20:40:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,27 +5,25 @@ end
 
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def batch_badge_creator(attendees)
-badges = []
-attendees.each do |name|
-  badges << "Hello, my name is #{name}."
+  badges = []
+    attendees.each do |name|
+    badges << "Hello, my name is #{name}."
 end
-return badges
+  return badges
 end
-batch_badge_creator(attendees)
-
+  batch_badge_creator(attendees)
 batch_badge_creator(attendees)
 
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def assign_rooms(attendees)
-room_assignments = []
-counter = 1
-attendees.each do |name|
-room_assignments << "Hello, #{name}! You'll be assigned to room #{counter}!"
-counter += 1
+  room_assignments = []
+  counter = 1
+    attendees.each do |name|
+  room_assignments << "Hello, #{name}! You'll be assigned to room #{counter}!"
+  counter += 1
 end
-return room_assignments
+  return room_assignments
 end
-
 assign_rooms(attendees)
 
 def printer
